ea7fc6a8ee2339c01f61dad54b8b4737191e8a9a
unknown <guoyuquan@QL-01-0591.ql.com>
2018-03-30 17:04:51 +0800

    first output

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    app: './src/app.js',
-    vendor: './src/vendors.js'
-  },
+  entry: './src/index.js',
   output: {
-    filename: 'bundle.js'
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   mode: 'development'
 };
