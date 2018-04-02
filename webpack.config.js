7aaa399f8cabe49014d9d1d2d734b7e88ba2c48a
unknown <guoyuquan@QL-01-0591.ql.com>
2018-04-02 11:51:42 +0800

    dev server

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
       title: 'Output management2'
     })
   ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
